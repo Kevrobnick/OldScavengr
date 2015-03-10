a4c17a55cea0910feb7a5b58a71a41d07dd85385
Kevin Dickerson <kmdickerson6@gmail.com>
2015-03-10 14:34:58 -0400

    CreateGameButton now takes you to the CreateGameActivity screen. Fixed error with "dummybutton."

--- app/src/main/java/seniorproject/com/scavengr/MainActivity.java
+++ app/src/main/java/seniorproject/com/scavengr/MainActivity.java
@@ -125,7 +125,7 @@ public class MainActivity extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
